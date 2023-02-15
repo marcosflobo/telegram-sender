300ac288a97cd955d258af8d46e437031c6973af
Marcos F. Lobo <5293167+marcosflobo@users.noreply.github.com>
2023-02-15 22:45:29 +0000

    Add echo mechanims

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # telegram-sender
+
 REST API that accepts messages and are sent to a Telegram group
 
 ## Run
+
 ### Via gradle
+
 ```shell
 export TELEGRAM_TOKEN=foo TELEGRAM_USERNAME=test_snowways_bot && ./gradlew run
 ```
 
 ## Links
-- https://core.telegram.org/bots/tutorial
+
+- <https://core.telegram.org/bots/tutorial>
--- src/main/java/com/marcosflobo/telegram/Bot.java
+++ src/main/java/com/marcosflobo/telegram/Bot.java
@@ -3,15 +3,17 @@ package com.marcosflobo.telegram;
 import io.micronaut.context.annotation.Property;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 public class Bot extends TelegramLongPollingBot {
 
-  @Property(name = "{telegram.username}")
+  @Property(name = "telegram.username")
   private String username;
 
-  @Property(name = "{telegram.token}")
+  @Property(name = "telegram.token")
   private String token;
 
   @Override
@@ -32,7 +34,22 @@ public class Bot extends TelegramLongPollingBot {
     var msg = update.getMessage();
     var user = msg.getFrom();
 
-    log.info(user.getFirstName() + " wrote " + msg.getText());
+    System.out.println(user.getFirstName() + " wrote " + msg.getText());
+
+    var id = user.getId();
+
+    sendText(id, "Copy that, " + user.getFirstName() + "!");
   }
+
+  public void sendText(Long who, String what){
+    SendMessage sm = SendMessage.builder()
+            .chatId(who.toString()) //Who are we sending a message to
+            .text(what).build();    //Message content
+    try {
+        execute(sm);                        //Actually sending the message
+    } catch (TelegramApiException e) {
+        throw new RuntimeException(e);      //Any error will be printed here
+    }
+}
 
 }
